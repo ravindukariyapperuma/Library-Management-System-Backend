604e9c21443fb89ba0ff12bc2fb24e4ed32a4c6e
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2021-09-17 16:51:50 +0530

    attributes add for book model

--- src/main/java/com/example/librarymanagement/model/Book.java
+++ src/main/java/com/example/librarymanagement/model/Book.java
@@ -1,4 +1,21 @@
 package com.example.librarymanagement.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "book_name", nullable = false)
+    private String bookName;
+
+    @Column(name = "author")
+    private String author;
 }
